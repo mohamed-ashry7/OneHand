17ca1c8b8760f4c7de5e144b8e94de45791d8532
MaherMohammed <47296887+MaherMohammed@users.noreply.github.com>
2019-12-02 16:55:54 +0200

    user model is finished

---
+++ root/backend/models/User.js
@@ -0,0 +1,32 @@
+const mongo = require('mongoose');
+const schema = mongo.Schema;
+
+const UserSchema = new schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+});
+
+module.exports = User = mongo.model('Users',UserSchema);
