231d0f311a340c38faf45fb7466503f5eee6dc11
ashry <mohamedelashry7777@gmail.com>
2019-12-09 15:51:30 +0200

    adding compose-up file and env

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,17 +1,20 @@
 
 
 const express = require('express')
-const mongoose = require('mongoose')
-const config = require('./config/keys')
-const cors = require("cors")
-const app = express()
+const mongoose = require('mongoose');
+const config = require('./config/keys');
+const cors = require("cors");
+const app = express();
 const users = require("./routes/api/users");
 const items = require("./routes/api/items");
 const notifications = require("./routes/api/notifications");
 const offerRequests = require("./routes/api/offerRequests");
 const stripePayment = require('./routes/api/stripeApi') ; 
 const bodyParser = require('body-parser');
-
+const dotenv=require('dotenv') ;
+dotenv.config({
+    path:require('find-config')('.env') 
+});
 // Connecting to DataBase 
 mongoose.connect(config.mongoURI , { useNewUrlParser: true 
     ,useUnifiedTopology: true 
